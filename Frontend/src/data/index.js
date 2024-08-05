1a9f35e0fc61366f894de0eee8a05c8473b6f0f9
Divaskar <130750966+Divaskar068@users.noreply.github.com>
2024-08-05 10:01:32 +0400

    Add files via upload

--- Frontend/src/data/index.js
+++ Frontend/src/data/index.js
@@ -1,1 +1,37 @@
+const employeesData = [
 
+    {
+
+        id: 1,
+       Name: 'Naman',
+        Role: 'Dewan',
+        email: 'namandewan971@gmail.com'
+        
+    }  ,
+    {
+
+        id: 2,
+       Name: 'Ashwin',
+        Role: 'gurumurthi',
+        email: 'f20200270@dubai-bits.pilani.ac.in'
+      
+    },
+    {
+
+        id: 3,
+       Name: 'Sanskriti',
+        Role: 'Khandelwal',
+        email: 'f2020243@dubai-bits.pilani.ac.in'
+        
+
+    },
+    {
+
+        id: 4,
+       Name: 'Divaskar',
+        Role: 'Anand',
+        email: 'f20200068@dubai-bits.pilani.ac.in'
+        
+    }
+];
+export { employeesData };
